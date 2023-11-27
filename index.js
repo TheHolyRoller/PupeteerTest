54a42040c39c628f8a56a794b61ab834c7ffca4c
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2023-11-27 17:40:24 +0100

    Just creating a proof of concept

---
+++ index.js
@@ -0,0 +1,46 @@
+// Import puppeteer module
+const puppeteer = require('puppeteer');
+
+
+// Define an async function
+(async () => {
+
+
+  const browser = await puppeteer.launch();
+  // Create a new page
+  const page = await browser.newPage();
+  
+  const urls = [
+      'https://www.wix.com',
+      'https://www.notion.so',
+    'https://www.spareroom.co.uk',
+    'https://www.gmail.com'
+
+  ];
+  
+  let counter = 0;
+
+  const loop = async () => {
+    // Get the URL at the counter index
+    const url = urls[counter];
+    // Navigate to the URL
+    await page.goto(url);
+    // Perform your desired tasks
+    // For example, take a screenshot and save it as facebook.png
+    await page.screenshot({path: 'facebook.png'});
+    // Increment the counter
+    counter++;
+    // If the counter reaches the end of the array, reset it to zero
+    if (counter === urls.length) {
+      counter = 0;
+    }
+    // Call the loop function again after a delay
+    setTimeout(loop, 1000 * 60 * 60); // Delay of one hour
+  };
+  // Call the loop function for the first time
+  loop();
+  
+  setInterval(loop, 1000 * 60 * 60); // Interval of one hour
+
+  
+})();
